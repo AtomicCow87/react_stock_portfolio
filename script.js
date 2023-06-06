c34e533aadd9360aba533af05628085091766b3f
Zach <zkdoran@gmail.com>
2023-06-06 18:23:16 -0400

    resetting js/css

--- script.js
+++
@@ -1,7 +0,0 @@
-var element = React.createElement(
-  "h1",
-  { className: "greeting" },
-  "Hello world!"
-);
-
-ReactDOM.render(element, document.getElementById('root'));
--- src/script.js
+++
@@ -1,6 +0,0 @@
-const element = <h1 className="greeting">Hello world!</h1>
-
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
